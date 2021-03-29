41a1a4c8d2d8be0fafd71479a8e8923cd438a6d4
boburgoziyev3107-gmail-com <boburgoziyevmm62@gmail.com>
2021-03-29 16:52:49 +0500

    task1.5

---
+++ task_1.5/university/courses/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class Speciality(models.Model):
+    is_active = models.BooleanField(default=True)
+    name = models.CharField(max_length=250)
+    code = models.IntegerField()
+    start_date = models.DateTimeField()
+
+
+class Teacher(models.Model):
+    HIGH = "high"
+    MIDDLE = "middle"
+    LOW = "low"
+
+    LAVEL = (
+        (HIGH, "High"),
+        (MIDDLE, "Middle"),
+        (LOW, "low")
+    )
+
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    degree = models.CharField(max_length=10, choices=LAVEL)
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=512)
+    speciality = models.ForeignKey(Speciality, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
