8b4de76459c072f52168a197eb339e8946545367
boburgoziyev3107-gmail-com <boburgoziyevmm62@gmail.com>
2021-04-03 00:37:18 +0500

    task 1.6 bajarildi va screenshot qilindi

--- task_1.5/university/courses/models.py
+++ task_1.5/university/courses/models.py
@@ -5,7 +5,10 @@ class Speciality(models.Model):
     is_active = models.BooleanField(default=True)
     name = models.CharField(max_length=250)
     code = models.IntegerField()
-    start_date = models.DateTimeField()
+    start_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Teacher(models.Model):
@@ -23,8 +26,14 @@ class Teacher(models.Model):
     last_name = models.CharField(max_length=250)
     degree = models.CharField(max_length=10, choices=LAVEL)
 
+    def __str__(self):
+        return f"{self.last_name} {self.degree} lavel"
+
 
 class Subject(models.Model):
     name = models.CharField(max_length=512)
     speciality = models.ManyToManyField(Speciality)
     teacher = models.ManyToManyField(Teacher)
+
+    def __str__(self):
+        return f"{self.name} science"
