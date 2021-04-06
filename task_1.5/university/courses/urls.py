1dfc51646befec9af911e8cea6ba95683fe5e879
boburgoziyev3107-gmail-com <boburgoziyevmm62@gmail.com>
2021-04-06 15:40:56 +0500

    global urlda o'zgarishlar kiritildi

--- task_1.5/university/courses/urls.py
+++ task_1.5/university/courses/urls.py
@@ -3,9 +3,9 @@ from .views import *
 
 urlpatterns = [
     # path('courses/<str:name>/', home)
-    path('courses/', home),
-    path('courses/speciality/', special, name='special_list'),
-    path('courses/teacher/', teacher, name='teacher'),
-    path('courses/subject/', subjects, name='subject'),
-    path('courses/subjet/<int:pk>/', subject_detail, name='subject-detail'),
+    path('', home),
+    path('speciality/', special, name='special_list'),
+    path('teacher/', teacher, name='teacher'),
+    path('subject/', subjects, name='subject'),
+    path('subjet/<int:pk>/', subject_detail, name='subject-detail'),
 ]
--- task_1.5/university/university/urls.py
+++ task_1.5/university/university/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('courses.urls'))  #task1.7_1
+    path('courses/', include('courses.urls'))  #task1.7_1
 ]
