867a153721ebe6f695a9a3d2de33fa5ba13cc8b6
boburgoziyev3107-gmail-com <boburgoziyevmm62@gmail.com>
2021-04-15 12:40:19 +0500

    task_9 was done

--- task_1.5/university/courses/admin.py
+++ task_1.5/university/courses/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from .models import Speciality, Teacher, Subject
 
-admin.site.register(Speciality)
-admin.site.register(Teacher)
-admin.site.register(Subject)
+
+# admin.site.register(Speciality)
+# admin.site.register(Teacher)
+# admin.site.register(Subject)
+
+
+@admin.register(Speciality)
+class SpecialityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'is_active',)
+    search_fields = ('name', 'code',)
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'degree',)
+    search_fields = ('first_name', 'last_name',)
+    list_filter = ('degree',)
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    autocomplete_fields = ['speciality', 'teacher']
+    search_fields = ('name',)
